28068925763d728a61e129b9d2fa0a879c5fce9b
sdim-ua <ingersol@yandex.ua>
2019-01-30 00:53:05 +0400

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,23 @@
+/* 
+Основа колеса
+*/
+
+public class Circle {
+    public double x;
+    public double y;
+    public double r;
+    
+    public Circle (double x, double y, public double r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    }
+    
+    
+    
+
+    //напишите тут ваш код
+    public static void main(String[] args) {
+
+    }
+}
